dde676fdca0eb298ab119de5ca3a19cabe8d7d59
Francisco Alcalde <francisco@alcalde.com.ar>
2025-01-28 19:55:39 -0300

    exercise0.6

--- 0_randomness/exercise0.6/walker_levy_flight.gd
+++ 0_randomness/exercise0.6/walker_levy_flight.gd
@@ -1,7 +1,5 @@
 extends Node2D
-# Achieved by making more possible steps to the right and down
-# than steps left and up.
-# Could also be done by giving bigger chances of right and down.
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,8 +8,19 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	var stepx: int = randi_range(-1, 2)
-	var stepy: int = randi_range(-1, 2)
+	var stepx: float = accept_reject()
+	var stepy: float = accept_reject()
 	var step: Vector2 = Vector2(stepx, stepy)
-	
+
 	self.global_position += step
+
+
+func accept_reject() -> float:
+	var rand_num: float = randf_range(-1.0, 1.0)
+	var qualify_proba: float = sqrt(abs(rand_num))
+	var qualify_res: float = randf()
+	
+	if qualify_res < qualify_proba:
+		return rand_num
+	
+	return accept_reject()
